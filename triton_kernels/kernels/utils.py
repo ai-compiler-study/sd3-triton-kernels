50361cf607a245a50c9516a6acef3e7a716cc577
sjjeong94 <sjjeong94@gmail.com>
2025-07-07 22:11:40 +0900

    add a util function to write ir

--- triton_kernels/kernels/utils.py
+++ triton_kernels/kernels/utils.py
@@ -1,4 +1,8 @@
+import os
+from typing import Any, Dict
+
 import triton
+from triton.tools.disasm import get_sass
 
 
 def calculate_settings(n):
@@ -17,3 +21,18 @@ def calculate_settings(n):
     elif BLOCK_SIZE >= 2048:
         num_warps = 8
     return BLOCK_SIZE, num_warps
+
+
+def write(txt: str, file_path: str) -> None:
+    with open(file_path, "w") as f:
+        f.write(txt)
+
+
+def write_ir(ir_dict: Dict[str, Any], path: str) -> None:
+    os.makedirs(path, exist_ok=True)
+    for key, value in ir_dict.items():
+        if key == "cubin":
+            sass = get_sass(value)
+            write(sass, os.path.join(path, f"{key}.mlir"))
+        else:
+            write(value, os.path.join(path, f"{key}.mlir"))
